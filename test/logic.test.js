9ffe3487a27fd702a7fd43caa500d2cfaf5cdbb3
Mateusz Ostafil <mostafil@divante.com>
2021-09-30 12:13:42 +0200

    Add tests

--- test/logic.test.js
+++ test/logic.test.js
@@ -1,6 +1,14 @@
-import {isAdult} from '../src/logic';
+import {isAdult, isAllowedToBuyBeer} from '../src/logic';
 
 test('30 yo in PL is adult', () => {
   expect(isAdult(30, 'PL')).toBe(true)
+})
+test('full coverage', () => {
+  isAdult(30, 'PL')
+  isAdult(30, 'USA')
+  isAdult(30, 'DE')
+  isAdult(30, '')
+  isAllowedToBuyBeer(10, 0, 'PL')
+  isAllowedToBuyBeer(10, 6, 'PL')
 })
 
